d632a543cc21ac07f893a4c574c066f4003e774e
tombo_gokuraku <tombo.gokuraku.1040@gmail.com>
2019-05-30 12:45:48 +0900

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='mongodelta',
     version='0.0.1',
     py_modules=['mongodelta'],
-    description='The mongodelta is encoder and decoder for handling datetime.timedelta object with MongoDB.'
-    url='https://github.com/tombo-gokuraku/mongodelta'
-    author='tombo-gokuraku'
+    description='The mongodelta is encoder and decoder for handling datetime.timedelta object with MongoDB.',
+    url='https://github.com/tombo-gokuraku/mongodelta',
+    author='tombo-gokuraku',
 )
